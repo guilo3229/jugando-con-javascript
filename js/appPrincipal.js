21492a4fa5621f853b8b3eaa8b4a86289c2f003b
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-04 22:59:38 +0200

    Carrito de frutas version 1 con todo en while y usando interpolacion tutorial 2

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,5 +1,5 @@
 const frutas=[]
-
+// En este caso se podia hacer un for of para que leyese despues del while todo y lo fuese mostrando, es decir el primer promt para el primera fruta y luego rellenaba con mas frutas la constante frutas , posteriormente como se ve y luego el for of las iba leyendo y poniendolas en la consola
 const fruta = prompt ("bienvenido al mercado que tipo de fruta desea?")
 frutas.push(fruta)
 console.log(`compraste:  

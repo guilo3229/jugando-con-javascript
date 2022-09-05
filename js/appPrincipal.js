f32633b66339a9198227e93f55bfa1aff44e644a
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-05 10:04:47 +0200

    objetos literales1

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,10 +1,24 @@
-let frutas =["manzana","sandia","pera"]
-// For Each con index, las llaves se pueden omitir si definimos fruta y luego index en la consola habra que ponerlo al reves pero si ponemos index primero habra que ponerlo segundo en la consola ojo!!
-// Tambien se puede hacer añadiendole interpolacion para que quede mas bonito
-   
-frutas.forEach((fruta,index,array) =>{
-        console.log(`${index }: ${fruta}`)
-       
-        console.log(array)
-    }
-)
+// objetos literales
+// console.log("vinculado 😍")
+// llavecitas para un objeto
+// tiene atributos nombre,color, acciones puede comer etc
+// lo que separa la propiedad del valor se usa los 2 puntitos
+// acordarse de la coma para pasar a la sieguiente propiedad
+// no se puede poner los index ya que son propiedades
+// se puede poner con comillas o sin comillas
+const gato ={
+nombre: "valiente",
+duerme: true,
+edad: 10,
+enemigos:["agua", "perros"]
+
+}
+// para llamar una propiedad punto y la propiedad
+console.log(gato.nombre)
+// tambien puede sllamar una propiedad potniendo el bjeto y entre corchetes y comillas dicha propiedad
+console.log(gato["nombre"])
+console.log(gato.duerme)
+console.log(gato["enemigos"])
+// primer valor de la propiedad enemigos:
+console.log(gato["enemigos"][0])
+console.log(gato.enemigos[0])

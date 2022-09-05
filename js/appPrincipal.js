b74185381c7434c746f065566dee1e20af3ba0b2
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-05 10:19:51 +0200

    objetos crud 2 si existen o no existen dichas prop

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -11,10 +11,11 @@ enemigos:["agua", "perros"]
 
 }
 
-// crear nuevas propiedades:
-gato.color = "azul"
+// comprobar si existe o no la propiedad de un objeto
+// .hasOwnProperty(propname) determina  si un objeto tiene una propiedad con ese nombre. .hasOwnProperty() devuelve true o false si se encuentra la propiedad o no.
+// Parece ser que si es true en un if con una funcion como por ejemplo en este caso hasOwnProperty entra dentro del if si es false no lo realice teer en cuenta esto, es decir esta diciendo que si existe el nombre lo actualiza a flojera
+// recuerda el if siempre en minusculas si no se puede joder y con las demas funciones de serie igual cuidado!!
+if (gato.hasOwnProperty ("nombre")){
+    gato.nombre = "flojera"
+}
 console.log(gato)
-// actualizar
-gato.edad = 5
-// eliminar la propiedad
-delete gato.duerme

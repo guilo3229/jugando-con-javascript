5d3079e0f41ea2defe9f35d8ec143711aa3e6c81
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-05 18:53:02 +0200

    propiedades y metodos 5 metodos y dudas con funciones de flecha

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -9,7 +9,7 @@ const gato ={
     edad: 10,
     enemigos:["agua", "perros"],
     // funcion de flecha dentro del objeto
-    comer: (alimento) => {
+    comer(alimento) {
                     // recordemos primero el objeto y luego la propiedad por eso gato.nombre,pero hay otra manera mas elegente de hacerlo con el famoso this!!
                     // this: Hace referencia al objeto contexto de JavaScript en el cual se está ejecutando el código actual(contexto el scout rodo dentro del gato en este caso)
                     // con flecha no se puede porque hay un this dentro de la flehca y te sale undefined
@@ -17,12 +17,20 @@ const gato ={
             ${this.nombre} esta comiendo ${alimento}
         `)
         
-    }
+    },
+    // como ya sabemos el for each hace un recorrido
+    listarEnemigos(){
+    // en este caso se puede usar la funcion de flecha dentro de nuestro metodo, pero no como un metodo.
+        this.enemigos.forEach((item) => console.log(item));
+
+    },
 };
+
+gato.listarEnemigos();
 
 
 // tener en cuenta que si ponemos string siempre entre corchetes y si no es es strin osea numero tal cual sin corchetes es una variable sin mas
-gato.comer("pez")
+// gato.comer("pez")
 
 
 

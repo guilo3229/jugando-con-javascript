4aa91273e73c4a645ac7afeda0056bf999133639
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-05 09:41:18 +0200

    for each 2

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,16 +1,10 @@
-const frutas = []
-const fruta = prompt('🍒 Feria Market 🍉 ¿qué fruta desea comprar?')
-
-frutas.push(fruta)
-
-while (confirm('¿Desea agregar otro elemento al 🛒?')) {
-    const fruta = prompt('¿qué fruta desea comprar?')
-    frutas.push(fruta)
-}
-
-console.log('Ustede compró: ')
-// for (let fruta of frutas) {
-//     console.log(fruta)
-// }
-// recibe una funcion que va a retomar un elemento, se puede poner el index detro del for each
-frutas.forEach((fruta) => console.log(fruta))
+let frutas =["manzana","sandia","pera"]
+// For Each con index, las llaves se pueden omitir si definimos fruta y luego index en la consola habra que ponerlo al reves pero si ponemos index primero habra que ponerlo segundo en la consola ojo!!
+// Tambien se puede hacer añadiendole interpolacion para que quede mas bonito
+   
+frutas.forEach((fruta,index,array) =>{
+        console.log(`${index }: ${fruta}`)
+       
+        console.log(array)
+    }
+)

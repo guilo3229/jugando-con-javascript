1d3c2bd446b985d81a2406257e57843aa25a8daf
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 12:06:01 +0200

    Destructuring anidados

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -3,11 +3,19 @@ const gato ={
     // nombre: "valiente",
     duerme: true,
     edad: 10,
-    enemigos:["agua", "perros"]
+    enemigos:["agua", "perros"],
+    otros:{
+        amigos:["cobarde", "timido", "pegajoso"],
+        favoritos:{
+            fria: "salmon",
+            caliente: "pollo",
+        }
+    }
 }
-// destructuring es  para hacer esta operacion de aqui abajo de manera mucho mas sencilla
-const nombreGato = gato.nombre
-// el objeto debe ir despues del igual y entre las llaves la propiedad que queremos, si tenemos otra constante con el nombre de dicha propiedad 2 puntitos y le ponemos otro nombre para poder invocarla despues y que no de error es decir hacemos un alias
-// si no existe la propiedad nombre usamos la propeidad por defecto es decir le decimos a nombre que sacara en consla si no tiene la propeidad dicha y probamos poniendo en texto dicha propiedad
-const{ nombre: nombreSuperGato = "sin  nombre", duerme: duermeGato, edad} = gato
-console.log(nombreSuperGato, duermeGato, edad)
+// primera forma de desestructuracion anidada que funciona
+const{amigos: amigosGatos} = gato.otros;
+console.log(amigosGatos);
+// otra forma de desestructuracion que funciona
+// la primera forma que esta mas arriba es mejor que la inmediata de aqui abajo
+// const {otros: {amigos: amigosGatos}} = gato;
+// console.log(amigosGatos);

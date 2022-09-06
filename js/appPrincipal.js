538ead2cc1257e038b1b32a9dfaf0f08d5ffce71
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 11:40:43 +0200

    destructuring 1

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -5,11 +5,8 @@ const gato ={
     edad: 10,
     enemigos:["agua", "perros"]
 }
-// Este comando por decirlo de alguna manera devuelve un array correspondiente a las propiedades enumerables de un objeto es el Object.values("aqui el bnombre del objeto en este caso gato")
-console.log(Object.values(gato))
-// Otra forma para hacerlo , se puede utilizar el objetct+ for each +funcion de flecha, esto solo sirve como podemos ver en la consola 
-Object.values(gato).forEach((i) => console.log(i))
+// destructuring es  para hacer esta operacion de aqui abajo de manera mucho mas sencilla
+const nombreGato = gato.nombre
 
-// hay otros metodos como:Object.entries()
-// Object.key()
-// Object.getOwnPropertyNames()
+const{ nombre: nombreSuperGato, duerme, edad} = gato
+console.log(nombreSuperGato, duerme, edad)

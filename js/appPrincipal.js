0f4f437f9fa024ac4f997b105efc17f7805da3d8
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 10:57:08 +0200

    objects values para ver los valores de las propiedades atraves de una array etc

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -5,9 +5,11 @@ const gato ={
     edad: 10,
     enemigos:["agua", "perros"]
 }
-// usamos el for in y no el for of porque este ultimo era para los array y esto es un objeto con sus propiedades es diferente!!
-// poner en corchete para que salgan las  definiciones de las propiedades es decir lo que esta a su derecha y no el nombre de las propiedades.
-for(let propiedad in gato){
-    console.log(gato[propiedad])
-    // console.log(gato.propiedad)--> no funciona apareceria undefined
-}
+// Este comando por decirlo de alguna manera devuelve un array correspondiente a las propiedades enumerables de un objeto es el Object.values("aqui el bnombre del objeto en este caso gato")
+console.log(Object.values(gato))
+// Otra forma para hacerlo , se puede utilizar el objetct+ for each +funcion de flecha, esto solo sirve como podemos ver en la consola para que nos muestre simplemente los string dentro del objeto en este caso
+Object.values(gato).forEach((i) => console.log(i))
+// Pr ultimo se puede obserbar que aunque la propiedad nombre es un string la omite ya que sale en la anterior consola si no tuviesemos la anterior linea a este object osea la que aparece como console mas arriba apareceria mismamente el nombre
+// hay otros metodos como:Object.entries()
+// Object.key()
+// Object.getOwnPropertyNames()

0cedaedbb78dd1f1859d2da3074b1fb4478a7c68
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 19:06:29 +0200

    DOMCONTENt loaded problema scrypt 5

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,2 +1,7 @@
-console.log(document.getElementById("tituloWeb"))
-console.log(document.getElementById("tituloWeb").textContent)
+
+
+// Con esto estamos diciendo carga el DOM y ejecuta con neustra funcion de flecha todo lo de las consoles en este CacheStorage, asi no hara falta poner justo al final del body nuestro scrypt esto es una alternativa
+document.addEventListener("DOMContentLoaded",() =>{
+    console.log(document.getElementById("tituloWeb"))
+    console.log(document.getElementById("tituloWeb").textContent)
+})

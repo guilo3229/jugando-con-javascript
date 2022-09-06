4d9bbfca5501d8723cfefd5c3d54118bae153a6a
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 11:03:04 +0200

    objects para ver los valores el anterior commit estaba mal este esta bien

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -7,9 +7,9 @@ const gato ={
 }
 // Este comando por decirlo de alguna manera devuelve un array correspondiente a las propiedades enumerables de un objeto es el Object.values("aqui el bnombre del objeto en este caso gato")
 console.log(Object.values(gato))
-// Otra forma para hacerlo , se puede utilizar el objetct+ for each +funcion de flecha, esto solo sirve como podemos ver en la consola para que nos muestre simplemente los string dentro del objeto en este caso
+// Otra forma para hacerlo , se puede utilizar el objetct+ for each +funcion de flecha, esto solo sirve como podemos ver en la consola 
 Object.values(gato).forEach((i) => console.log(i))
-// Pr ultimo se puede obserbar que aunque la propiedad nombre es un string la omite ya que sale en la anterior consola si no tuviesemos la anterior linea a este object osea la que aparece como console mas arriba apareceria mismamente el nombre
+
 // hay otros metodos como:Object.entries()
 // Object.key()
 // Object.getOwnPropertyNames()

f5bbd90983ca7a98d3758263a7d4d8e3daf5ac25
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 18:41:43 +0200

    DOM GetElementby ID 2

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,16 +1,1 @@
-console.log("probando😍");
-
-console.log(document);
-// esto es como esta siendo representado nuestro archivo estatico index html en el navegador, se puede acceder a lementos etc
-// las propiedades se llaman sin mas
-console.log(document.head);
-console.log(document.title);
-// algunos metodos
-// Document.getElementsByClassName(String className)
-// Document.getElementsByTagName(String tagName)
-// Document.getElementById(String id)
-// Document.querySelector(String selector)
-// Document.querySelectorAll(String selector)
-// Document.createDocumentFragment()
-// Document.createElement(String name)
-
+console.log(document.getElementById("tituloWeb"))

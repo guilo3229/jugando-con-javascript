7e15c7b9a1559f2867072ef2fae582d13b5788a5
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 13:06:59 +0200

    por valor que muestra

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,32 +1,5 @@
-
-const gato ={
-    nombre: "valiente",
-    duerme: true,
-    edad: 10,
-    enemigos:["agua", "perros"],
-    // los get no reciben parametros nada dentro de los parentesis
-    get nombreMayuscula(){
-        return this.nombre.toUpperCase()
-    },
-    // El set toma un solo parametro y puede modificar los valores que tiene esa propiedad
-    set agregarEnemigo(nuevoEnemigo){
-        this.enemigos.push(nuevoEnemigo)
-    }
-    
-}
-
-// para llamar al set es un poco diferente como veremos
-gato.agregarEnemigo = "batman"
-console.log(gato)
-
-console.log(gato.nombreMayuscula)
-
-// get : Enlaza la propiedad de un objeto con una función que será llamada cuando la propiedad es buscada.
-// set : La sintaxis set asocia la propiedad de un objeto a una función que será llamada cuando haya un intento de asignar valor a esa propiedad.
-// GET: Tenga en cuenta lo siguiente al trabajar con la sintaxis get:
-
-// Debe tener exactamente cero parametros.
-// No debe haber múltiples getters para una misma propiedad.
-// SET: Tenga en cuenta lo siguiente al trabajar con setters:
-
-// Debe tener exactamente un parámentro
+let a = 'hola'
+let b = a;
+// se paso por valor
+ a = 'chao';
+console.log(b)

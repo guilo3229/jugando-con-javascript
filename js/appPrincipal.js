38b5b0e6c18cf650291782df11692628bafc8ee5
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-06 12:36:07 +0200

    destrcturing no funciona con el this el nombre

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,21 +1,17 @@
 
 const gato ={
-    // nombre: "valiente",
+    nombre: "valiente",
     duerme: true,
     edad: 10,
     enemigos:["agua", "perros"],
-    otros:{
-        amigos:["cobarde", "timido", "pegajoso"],
-        favoritos:{
-            fria: "salmon",
-            caliente: "pollo",
-        }
-    }
+    comer(comida) {
+        return `${this.nombre} está comiendo ${comida}`;
+    },
+    mostrarEnemigos() {
+        return this.enemigos.forEach((item) => console.log(item));
+    },
 }
 
-
-const{amigos: amigosArray} = gato.otros;
-// destructuracion de un array, uso corchetes porque es un array, definimos los elementos que queremos dentro de los corchetes
-// es decir sacamos la propiedad a amigosArray, esto es un array que sacaremos, lo que queramos en una constante y despues invocamos dicha constante con el console si queremos uno o varios elementos de esta ultima
-const[aUno, aDos, aTres, aCuatro = "no existe"] = amigosArray
-console.log(aUno,aDos);
+const comer =gato.comer
+//  no puedes usar el destructuring con el this!!
+console.log(comer("pez"))

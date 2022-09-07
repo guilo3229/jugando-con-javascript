f604b8a16aa6ae3c2f402b43447607b797e98d86
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-07 11:30:24 +0200

    eventos addevent importante

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,14 +1,9 @@
-// muchas veces los elmentos los guardabmos en una variable
 const h1 = document.getElementById("tituloWeb")
-// hacemos una constante del elemento para tener mas facilidad a la hora de trabajar con el
-console.log(h1.id)
-console.log(h1.style)
-console.log(h1.tagName)
-console.log(h1.textContent)
-// todas son propiedades ya que no tienen corchetes
-// tambien se puede modificar la propiedad del elemento, le ponen el antes del nombre
-h1.textContent = "nuevo text desde js"
-// modificando todo atraves de javascript
-h1.style.backgroundColor = "red"
-h1.style.color = "white"
-un beneficio de capturarlo en una variable es que podemos modificar los elementos de manera facil.
+// normalmente con button es mejor usar eById pero vamos a probar con todos
+const boton = document.querySelector(".btn-primary")
+// seleccionamos el evento y una vez que este seleccinado le metemos la aplicacion
+boton.addEventListener("click", () => {
+    console.log("me diste click");
+    h1.textContent = "texto desde js"
+    h1.style.color = "red"
+})

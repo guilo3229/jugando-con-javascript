119bc39592cabd6d66370250992b922b21ad3430
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-07 17:46:45 +0200

    fragment 1

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,14 +1,13 @@
 const lista =document.querySelector("#lista")
 const arrayPaises =["peru","bolivia","colombia"]
-// En este caso si se puede , se esta repitiendo el ciclo pero es una array
-// arrayPaises.forEach(pais=>{
-//     const li =document.createElement("li")
-//     li.textContent = pais
-//     lista.appendChild(li)
-// })
+// Es un metodo y por lo tanto lleva parentesis
+const fragment = document.createDocumentFragment()
 
-// si no le pones un + antes del igual sobreescribe uno a uno en este caso
-arrayPaises.forEach((pais) =>{
-    lista.innerHTML += `<li>${pais}</li>`
+arrayPaises.forEach(pais=>{
+    const li =document.createElement("li")
+    li.textContent = pais
+    fragment.appendChild(li)
 })
-// mucho cuidado porque con el innerHTML se crea un Reflow y hace que puede haber errores y hace que vaya mas lento y tiene problemas de seguridad y lo puede meterle codigo maliciosos
+
+lista.appendChild(fragment)
+// Se podra  hacer que la lista se lea de final ahacia atras?? si con insert Before

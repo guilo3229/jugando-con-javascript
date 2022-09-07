58d29c6a69ee6709c5b6bf4944d58f49c764aa54
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-07 17:13:06 +0200

    Create elment 1

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -6,4 +6,7 @@ console.log(li)
 // ahora que ya esta la consola tenemos que empujarlo a nuestro DOM HTML
 // ahora identificado el nodo PannerNode(el ID)
 lista.appendChild(li)
-lista.appendChild(li)
+// Si el child hace una referencia a un nodo existente en el documento, el método appendChild se mueve de su posición actual a su nueva posición.
+// Ésto significa que el nodo no puede estar en dos puntos del documento de manera simultánea.
+// Así que si el nodo ya contiene un padre, primero es eliminado, y después se añade a la nueva posición.
+// Se puede usar Node.cloneNode (opens new window)para hacer una copia del nodo antes de añadirlo debajo de su nuevo elemento padre.

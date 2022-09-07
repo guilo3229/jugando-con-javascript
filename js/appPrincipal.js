211417617191544f51fa9e560440ca6a48883247
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-07 14:09:43 +0200

    practica V1 DOM 1

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,9 +1,14 @@
-const h1 = document.getElementById("tituloWeb")
-// normalmente con button es mejor usar eById pero vamos a probar con todos
-const boton = document.querySelector(".btn-primary")
-// seleccionamos el evento y una vez que este seleccinado le metemos la aplicacion
-boton.addEventListener("click", () => {
-    console.log("me diste click");
-    h1.textContent = "texto desde js"
-    h1.style.color = "red"
+const inputColor = document.querySelector("#inputColor")
+const btnVisualizar = document.querySelector("#btnVisualizar")
+const parrafoExa = document.querySelector("#parrafoExa")
+const cardColor = document.querySelector("#cardColor")
+
+// console.log(inputColor)
+// console.log(btnVisualizar)
+// console.log(parrafoExa)
+// console.log(cardColor)
+
+btnVisualizar.addEventListener("click", ()=>{
+
+    console.log("me diste click")
 })

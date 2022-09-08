a15e5f47b4dd5f3951161b6872f60e47a2b4715f
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-08 11:18:03 +0200

    Wteamplate 3

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,11 +1,19 @@
 const lista =document.querySelector("#lista")
-// const arrayPaises =["peru","bolivia","colombia"]
-// const fragment = document.createDocumentFragment();
-// vale aqui lo que hace, vemos el template que tiene ya un document, sacamos el query slector y luego clonamos dicho contenido y le ponemos true
+const fragment = document.createDocumentFragment();
 const liTemplate = document.querySelector('#liTemplate')
-const clone = liTemplate.content.cloneNode(true)
-// despues de colonarlo nos metemos dentro del objeto y modificamos la clase del span metiendonos dentro y poniendo textcontent para modificar dicho text
-clone.querySelector(".text-primary").textContent = "agregue a atraves de un template"
+const arrayPaises =["peru","bolivia","colombia"]
 
-// es decir hacemos un liTemplate, luego lo podemos definir en javascript modificarlo etc y luego lo inserrtamos en nuestro html, en este caso en el ul que tenemos para ello lo hacemos con appendchild de lista y asi lo insertamos.
-lista.appendChild(clone)
+arrayPaises.forEach((pais)=>{
+
+    const clone =liTemplate.content.cloneNode(true)
+    clone.querySelector('span').textContent = pais
+
+    fragment.appendChild(clone)
+})
+lista.appendChild(fragment)
+// const clone = liTemplate.content.cloneNode(true)
+
+// clone.querySelector(".text-primary").textContent = "agregue a atraves de un template"
+
+
+// lista.appendChild(clone)

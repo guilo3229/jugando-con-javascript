dc76896a662f4bb250a87ee0cb58c9d50ad873d2
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-08 09:51:58 +0200

    fragmenta a template

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,27 +1,29 @@
 const lista =document.querySelector("#lista")
 const arrayPaises =["peru","bolivia","colombia"]
 const fragment = document.createDocumentFragment();
-{/* <li class="list">
-            <b>Pais:</b>
-            <span class= text-primary">Aqui va elpais</span>        
-</li> */}
-
+// Lo hacemos como un template para que de vueltas y luego lo listamos con el comando HTML
+let template = ""
 arrayPaises.forEach((pais) =>{
+    template += `
+    <li class="list">
+    <b> Pais:</b>
+    <span class="text-primary">${pais}</span>        
+</li>`;
 
-    const li = document.createElement("li")
-    li.className = "lista"
+    // const li = document.createElement("li")
+    // li.className = "lista"
     
-    const b = document.createElement("b")
-    b.textContent = "pais: "
+    // const b = document.createElement("b")
+    // b.textContent = "pais: "
     
-    const span = document.createElement("span")
-    span.className= "text-primary"
-    span.textContent =pais
+    // const span = document.createElement("span")
+    // span.className= "text-primary"
+    // span.textContent =pais
 
-    li.appendChild(b)
-    li.appendChild(span)
-    fragment.appendChild(li)
+    // li.appendChild(b)
+    // li.appendChild(span)
+    // fragment.appendChild(li)
 
-})
-
-lista.appendChild(fragment)
+})   
+s
+lista.innerHTML = template

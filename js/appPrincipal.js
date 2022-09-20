68efe9aa9ca0b9fa46dd8e181eb9c3f65e369a38
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-20 17:55:45 +0200

    expresiones regulares2

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,14 +1,12 @@
-// Esto nos ayuda a encontrar dicha palabra en la expresion de caracteres no comillas
-// notacion literal:
-const regExp = /bluuweb/i;
-// puede instanciar un ojeto hacer una nueva instancia del objeto
-// notacion de objeto, en este caso si que estaria entrecomillado aunque arriba son barritas:
 
-// i Ignora mayúsculas y minúsculas. Se suele denominar insensible a mayús/minús.
-// g Búsqueda global. Sigue buscando coincidencias en lugar de pararse al encontrar una.
-// m Multilínea. Permite a ^ y $ tratar los finales de línea \r o \n
-// la RegExp de dentro tiene que ser diferente aa la constante de arriba es en mayuscula la primera diferente constante ojo sino no funciona
-const regExpObjeto = new RegExp("bluuweb", "i");
 
-// como evaluar, si la enceuntra verdadero sino falso encuentra esa palabra en una string aunque sea una frase dira que existe osea las letras tienen que existir seguidamente aunque sea una palabra incluso mayor es decir si buscamos la palabra codo y no existe esa palabra sola pero existe codomania junto nos dara true
-console.log(regExp.test("bl uuwebsdf"))
+// [ ] Rango de carácteres. Cualquiera de los caracteres del interior de los corchetes.
+// | Establece una alternativa: lo que está a la izquierda o lo que está a la derecha.
+const regExp = /[A-Za-z0-9]/i;
+
+
+// const regExpObjeto = new RegExp("bluweb|bluuweb", "i");
+
+
+// busca el u y el e por separado esta encontrando la u y la e con los corchetes
+console.log(regExp.test("*ñ5"))

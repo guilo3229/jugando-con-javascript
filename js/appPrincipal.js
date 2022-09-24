74745e4c712546a6b75f569f2912e9c2a89b98ae
guilo3229 <juanpablotorresperezuni@gmail.com>
2022-09-24 22:46:40 +0200

    funciones constructoras

--- js/appPrincipal.js
+++ js/appPrincipal.js
@@ -1,57 +1,20 @@
-// con query selector # para los id
+// Funcion constructora = Plantilla = Class
 
+function persona (nombre){
+    this.nombre = nombre
 
-// DOMContentLoadede esto espera a que carge todo el DOM
-document.addEventListener("DOMContentLoaded",() => {
-   fetchData()
-
-
-})
-
-const fetchData = async() => {
-    // console.log("obteniendo")
-    try{
-        loadingData(true)
-        const res = await fetch("https://rickandmortyapi.com/api/character")
-        const data = await res.json()
-        pintarCard(data)
-    } catch (error) {
-        console.log(error)
-
-
-    } finally{
-        loadingData(false)
-
-    }
-    
-}
-
-
-const pintarCard = data => {
-    const cards = document.getElementById("card-dinamicas")
-    const templateCard = document.getElementById("template-card").content
-    // Fragment para evitar el reflow
-    const fragment = document.createDocumentFragment()
-     console.log(data)
-     data.results.forEach(item => {
-        // console.log(item)
-        const clone = templateCard.cloneNode(true)
-        clone.querySelector("h5").textContent = item.name
-        clone.querySelector("p").textContent = item.species
-        clone.querySelector(".card-img-top").setAttribute("src", item.image)
-        fragment.appendChild(clone)
-     });
-     cards.appendChild(fragment)
-}
-
-const loadingData = (estado) =>{
-
-    const loading = document.getElementById("loading")
-    if(estado){
-        
-        loading.classList.remove("d-none")
-
-    }else {
-        loading.classList.add("d-none")
+    this.saludar = function(){
+            // Recuerda lo de la interpolacion 
+        return `${this.nombre} dice hola`
     }
 }
+// Esto es instanciar, creamos nuestra plantilla objeto a trabes de una funcion y le añadimos dinamicamente las variable que queramos a esa plantilla para no tener que ir haciendo los objetos uno a uno y de esta manera es mas sencillo y ahorramos codigo, recordar que no se peuden usar funciones de flecha porque utilizamos this!!!
+
+// Lo de arriba no es un objeto, es cuando lo instanciamos aqui abajo es cuando creamos los objetos
+const juanito = new persona("juanito")
+const pedrito = new persona("pedrito")
+
+console.log(juanito)
+console.log(pedrito)
+console.log(juanito.saludar())
+console.log(pedrito.saludar())
